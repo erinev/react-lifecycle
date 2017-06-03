563f7b7512a30b203f2ec52e55ca3eef79f62a05
Erikas Neverdauskas <e.neverdauskas@adform.com>
2017-06-03 09:40:19 +0300

    added working environment

---
+++ src/app/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>React lifecycle</h1>
+        <p>Learning react lifecycle with joy.</p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
